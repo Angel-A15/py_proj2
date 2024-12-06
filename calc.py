17e9f9953572fabee7dbe7032c62c56d8f1f02b9
Angel-A15 <aasupertwix15@gmail.com>
2024-12-05 19:32:07 -0600

    resolved syntax error issue, restyled structure to incorperate boolean that will simplify process to either run  code or error due to invalid operand

--- calc.py
+++ calc.py
@@ -2,28 +2,32 @@ operand = input("Number 1: ")
 operand2 = input("Number 2: ")
 sign = input("Sign: ")
 
+
+valid = False
 # If an invalid number(s) were to be input, this would catch it and
 # respond to display why; also cleans our code(DRY method)
 try:
     operand = float(operand)
     operand2 = float(operand2)
+    valid = True
 except:
     print("Ivalid operand")
 
-
-if sign == "+":
-    result = float(operand) + float(operand2)
-elif sign == "-":
-    result = float(operand) - float(operand2)
-elif sign == "/":
-    if float(operand2) != 0:
-        result = float(operand) / float(operand2)
-    else:
-        print("Division by zero.")
-elif sign == "*":
-    result = float(operand) * float(operand2)
-elif sign == "**":
-    result = float(operand) ** float(operand2)
-elif sign == "%":
-    result = float(operand) % float(operand2)
-print(result)
+if valid:
+    result = 0
+    if sign == "+":
+        result = operand + operand2
+    elif sign == "-":
+        result = operand - operand2
+    elif sign == "/":
+        if operand2 != 0:
+            result = operand / operand2
+        else:
+            print("Division by zero.")
+    elif sign == "*":
+        result = operand * operand2
+    # elif sign == "**":
+    #     result = operand **operand2
+    # elif sign == "%":
+    #     result = operand % operand2
+    print(result)
