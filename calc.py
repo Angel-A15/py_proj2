b4053d46412ea80bffbc6a0b6cad83b7c4bde5a5
Angel-A15 <aasupertwix15@gmail.com>
2024-12-06 21:05:19 -0600

    removed operand variables, converged them into while variable loops, and set them inside a function for a less stretched code structure and clean up

--- calc.py
+++ calc.py
@@ -1,17 +1,32 @@
-operand = input("Number 1: ")
-operand2 = input("Number 2: ")
+
+
+def get_number():
+    while True:
+        operand = input("Number 1: ")
+        try:
+            operand = float(operand)
+            break
+        except:
+            print("Invaled number, try again.")
+
+    while True:
+        operand2 = input("Number2: ")
+        try:
+            operand2 = float(operand2)
+            break
+        except:
+            print("Invalid number, try again.")
+
+
+
+
 sign = input("Sign: ")
 
 
 valid = False
 # If an invalid number(s) were to be input, this would catch it and
 # respond to display why; also cleans our code(DRY method)
-try:
-    operand = float(operand)
-    operand2 = float(operand2)
-    valid = True
-except:
-    print("Ivalid operand")
+
 
 if valid:
     result = 0
@@ -19,6 +34,10 @@ if valid:
         result = operand + operand2
     elif sign == "-":
         result = operand - operand2
+    elif sign == "**":
+        result = operand **operand2
+    elif sign == "%":
+        result = operand % operand2
     elif sign == "/":
         if operand2 != 0:
             result = operand / operand2
@@ -26,8 +45,5 @@ if valid:
             print("Division by zero.")
     elif sign == "*":
         result = operand * operand2
-    # elif sign == "**":
-    #     result = operand **operand2
-    # elif sign == "%":
-    #     result = operand % operand2
+
     print(result)
