84aeabfe1a5655e4ebac6db5d5fb720e493afbe0
Angel-A15 <aasupertwix15@gmail.com>
2024-12-06 21:23:53 -0600

    calcu works, issue of displaying number2 instead of number 1

--- calc.py
+++ calc.py
@@ -14,28 +14,25 @@ operand2 = get_number()
 
 sign = input("Sign: ")
 
-
-valid = False
 # If an invalid number(s) were to be input, this would catch it and
 # respond to display why; also cleans our code(DRY method)
 
+result = 0
+if sign == "+":
+    result = operand + operand2
+elif sign == "-":
+    result = operand - operand2
+elif sign == "**":
+    result = operand **operand2
+elif sign == "%":
+    result = operand % operand2
+elif sign == "/":
+    if operand2 != 0:
+        result = operand / operand2
+    else:
+        print("Division by zero.")
+elif sign == "*":
+    result = operand * operand2
 
-if valid:
-    result = 0
-    if sign == "+":
-        result = operand + operand2
-    elif sign == "-":
-        result = operand - operand2
-    elif sign == "**":
-        result = operand **operand2
-    elif sign == "%":
-        result = operand % operand2
-    elif sign == "/":
-        if operand2 != 0:
-            result = operand / operand2
-        else:
-            print("Division by zero.")
-    elif sign == "*":
-        result = operand * operand2
+print(result)
 
-    print(result)
