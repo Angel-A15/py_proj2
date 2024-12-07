ddc8be0e9697056013fb908602e07a34c78cadef
Angel-A15 <aasupertwix15@gmail.com>
2024-12-06 21:48:04 -0600

    added parameter to be used as string concatination to identify operand number order and passed getnumber function to operand vars with arguments to identify list

--- calc.py
+++ calc.py
@@ -1,22 +1,22 @@
-
-
-def get_number():
-    while True:
-        operand = input("Number 1: ")
-        try:
+# get_number function will gather input from user to define operands selected by user
+def get_number(number):
+    while True:     # Loop will repeat until two operands are selected by user 
+        operand = input("Number " + str(number) + ": ") # variable input will display how many/which number the user needs to fill in; based on parameter from function
+        try:            #If a valid operand is selected, the user will be asked to choose a number until function calls are used up
             return  float(operand)
         except:
+            # If theres an invalid number,this would catch it and tell the user to try a valid option
             print("Invaled number, try again.")
             
+# variables will each call the function with a listed argument
+operand = get_number(1)
+operand2 = get_number(2)
 
-operand = get_number()
-operand2 = get_number()
-
+#sign will be next to select 
 sign = input("Sign: ")
 
-# If an invalid number(s) were to be input, this would catch it and
-# respond to display why; also cleans our code(DRY method)
 
+# based on sign selected, operands will follow selected arithmatic operation 
 result = 0
 if sign == "+":
     result = operand + operand2
